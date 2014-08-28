9db46a4e7358cc474fef53840546d0974d37bfbb
Ensis <pierre.ecormier@yahoo.fr>
2014-08-28 14:46:27 +0200

    fix: compacter solution

--- dailyprogrammer/177/Intermediate/solve.py
+++ dailyprogrammer/177/Intermediate/solve.py
@@ -1,6 +1,5 @@
-import math
-import random
-import sys
+from math import sin
+from sys import argv
 import wave
 
 morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
@@ -8,19 +7,19 @@ morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
          "..-", "...-", ".--", "-..-", "-.--", "--.", "-----", ".----", "..---",
          "...--", "....-", ".....", "-....", "--...", "---..", "----.", "/"]
 
-if len(sys.argv) < 2:
+if len(argv) < 2:
     exit()
 
 def beep(f, sec, freq):
     vals = []
     for i in range(int(8000 * sec)):
-        vals.append(wave.struct.pack('h', math.sin((i / 8000.0) * 2 * 3.1415 * freq) * 128 * 0.4))
+        vals.append(wave.struct.pack('h', sin((i / 8000.0) * 6.2831 * freq) * 51.2))
     f.writeframes("".join(vals))
 
 f = wave.open("out.wav", "w")
 f.setparams((1, 2, 8000, 0, 'NONE', 'not compressed'))
 
-for c in "".join([morse[26 + ord(l) - ord('0') if (l >= '0' and l <= '9') else ord(l) - ord('a') if (l >= 'a' and l <= 'z') else -1] for l in sys.argv[1].lower()]):
+for c in "".join([morse[ord(l) - 22 if '0' <= l <= '9' else ord(l) - 97 if 'a' <= l <= 'z' else -1] for l in argv[1].lower()]):
     if c != "/":
         beep(f, [0.6, 0.2][c == '.'], 1000)
     beep(f, 0.2, 0)
