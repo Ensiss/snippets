078ab13b78410175e64890a4ba329e37da568d73
Ensis <pierre.ecormier@yahoo.fr>
2014-09-15 20:32:34 +0200

    add: error handling and optionnal seed

--- dailyprogrammer/180/Easy/solve.py
+++ dailyprogrammer/180/Easy/solve.py
@@ -1,6 +1,7 @@
 from itertools import groupby
+import sys
 
-r = "1"
-for i in range(int(__import__("sys").argv[1])):
+r = "1" if len(sys.argv) < 3 else sys.argv[2]
+for i in range(5 if len(sys.argv) < 2 else int(sys.argv[1])):
     r = "".join([str(len(s)) + s[0] for s in ["".join(s) for k, s in groupby(r)]])
 print r
