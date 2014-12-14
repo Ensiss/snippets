f46e7e3d543d2471ef3d150c59c84830091f56a3
Ensis <pierre.ecormier@yahoo.fr>
2014-12-14 23:47:18 +0000

    add: read source from file

--- c++/brainfuck/main.cpp
+++ c++/brainfuck/main.cpp
@@ -1,3 +1,7 @@
+#include        <sys/types.h>
+#include        <sys/stat.h>
+#include        <fcntl.h>
+#include        <cstdio>
 #include        <iostream>
 #include        <unistd.h>
 #include        <cstring>
@@ -5,13 +9,15 @@
 class           BrainFuck
 {
 public:
-  BrainFuck(const char *src, unsigned int mem = 4096)
-    : _pc(src), _mem(new char[mem]), _ptr(_mem) {}
-  BrainFuck() { delete[] _mem; }
+  BrainFuck(const char *src = NULL, unsigned int mem = 4096)
+    : _pc(src), _memsz(mem), _mem(new char[mem]), _ptr(_mem) {}
+  ~BrainFuck() { delete[] _mem; }
 
 public:
   void          step()
   {
+    if (!_pc)
+      return;
     if (*_pc == '>' || *_pc == '<')
       _ptr += *_pc - '=';
     else if (*_pc == '+' || *_pc == '-')
@@ -29,27 +35,47 @@ public:
     while (*_pc)
       step();
   }
+  void          run(const char *src)
+  {
+    memset(_mem, 0, _memsz);
+    _ptr = _mem;
+    _pc = src;
+    run();
+  }
 
 private:
   const char    *_pc;
+  unsigned int  _memsz;
   char          *_mem;
   char          *_ptr;
 };
 
 int             main(int ac, char **av)
 {
+  BrainFuck     bf;
   char          buff[4016];
+  int           fd, ret;
 
   if (ac < 2)
     {
-      int ret = read(0, buff, 4016);
+      ret = read(0, buff, 4016);
       if (ret && buff[ret - 1])
         buff[ret - 1] = '\0';
+      bf.run(buff);
     }
   else
-    strcpy(buff, av[1]);
-
-  BrainFuck     bf(buff);
-  bf.run();
+    {
+      for (int i = 1; i < ac; i++)
+        {
+          if ((fd = open(av[i], O_RDONLY)) == -1)
+            {
+              perror(av[i]);
+              continue;
+            }
+          if ((ret = read(fd, buff, 4096)) > 0)
+            bf.run(buff);
+          close(fd);
+        }
+    }
   return (0);
 }
